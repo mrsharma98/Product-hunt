8aae247ce38f3b37c16705961eed3db4c0d4da15
Manish Sharma <mrsharma98@gmail.com>
2020-03-19 14:49:40 +0530

    first commit

---
+++ producthunt/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from products import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home')
+
+]
